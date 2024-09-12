032ca46763408664f2f4bc7c7f1de2429401d164
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-09-13 00:44:09 +0500

    Task2

---
+++ Tyuiu.AlshinAF.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.AlshinAF.Task2.V0.Lib;
+namespace Tyuiu.AlshinAF.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Артур";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Артур", res);
+
+        }
+    }
+}
---
+++ Tyuiu.AlshinAF.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.AlshinAF.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Артур"));
+Console.ReadKey();
+
+
+
